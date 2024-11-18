3884722dbeb9e410f568cc94f336fa3fa14379a9
g-l-i-t-c-h-o-r-s-e <anne.hero88@gmail.com>
2024-11-18 05:24:36 -0600

    Update mangle.sh

--- mangle.sh
+++ mangle.sh
@@ -22,7 +22,7 @@ function printDependencies()
 {
     echo "Error: \"$1\" could not be found, but is required"
     echo ""
-    echo "This script requires ffmpeg and Sox to be installed"
+    echo "This script requires ffmpeg, Sox, and ImageMagick (magick) to be installed"
 
     cleanup 1
 }
@@ -47,11 +47,11 @@ function printEffects()
 
 function printHelp()
 {
-    echo "$ ./mangle.sh in.jpg out.png [effect [effect]]"
+    echo "$ ./mangle.sh in.jpg out.gif [effect [effect]]"
     echo ""
     echo "This script lets you interpret image or video data as sound,"
     echo "and apply audio effects to it before converting it back to"
-    echo "image representation"
+    echo "image representation as an animated GIF"
     echo ""
     echo "Options:"
     echo "--bits=X          -- Set audio sample size in bits, 8/16/24"
@@ -60,14 +60,15 @@ function printHelp()
     echo "--effects         -- Suggest some effects"
     echo "--help            -- Display this information"
     echo "--res=WxH         -- Set output resolution, 1920x1080"
+    echo "--framerate=X     -- Set GIF framerate (frames per second), default 10"
     echo ""
     printEffects
     echo ""
     echo "Examples:"
-    echo "./mangle in.jpg out.jpg vol 11"
-    echo "./mangle in.mp4 out.mp4 echo 0.8 0.88 60 0.4"
-    echo "./mangle in.mp4 out.mp4 pitch 5 --res=1280x720"
-    echo "./mangle in.mp4 out.mp4 pitch 5 --blend=0.75 --color-format=yuv444p --bits=8"
+    echo "./mangle.sh in.jpg out.gif vol 11"
+    echo "./mangle.sh in.mp4 out.gif echo 0.8 0.88 60 0.4"
+    echo "./mangle.sh in.mp4 out.gif pitch 5 --res=1280x720"
+    echo "./mangle.sh in.mp4 out.gif pitch 5 --blend=0.75 --color-format=yuv444p --bits=8 --framerate=15"
     echo ""
     echo "A full list of effects can be found here: http://sox.sourceforge.net/sox.html#EFFECTS"
 
@@ -100,6 +101,8 @@ function parseArgs()
     # Default values
     BITS=8
     YUV_FMT=rgb24
+    FRAMERATE=10
+    DELAY=$((100 / FRAMERATE))  # ImageMagick uses delay in 1/100s
 
     for i in "${@}"
     do
@@ -120,24 +123,22 @@ function parseArgs()
     do
     case $i in
         --res=*)
-            export RES=${i#*=}
-            RES_COLON=$(echo "$RES" | tr x :)
-            FFMPEG_IN_OPTS="$FFMPEG_IN_OPTS -vf scale=$RES_COLON"
+            RES=${i#*=}
         ;;
         --bits=*)
             BITS=${i#*=}
         ;;
         --blend=*)
             BLEND=${i#*=}
-            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS -f rawvideo -pix_fmt \$YUV_FMT -s \${RES} -i \${TMP_DIR}/tmp_audio_out.\${S_TYPE}"
-            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS -filter_complex \\\""
-            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS [0:v]setpts=PTS-STARTPTS, scale=\${RES}[top]\;"
-            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS [1:v]setpts=PTS-STARTPTS, scale=\${RES},"
-            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS format=yuva444p,colorchannelmixer=aa=${BLEND}[bottom]\;"
-            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS [top][bottom]overlay=shortest=1\\\""
+            # Blending is not directly handled by ImageMagick in this script version
+            echo "Warning: --blend option is not supported when creating GIFs with ImageMagick."
         ;;
         --color-format=*)
             YUV_FMT=${i#*=}
+        ;;
+        --framerate=*)
+            FRAMERATE=${i#*=}
+            DELAY=$((100 / FRAMERATE))
         ;;
         --help)
             printHelp
@@ -157,12 +158,14 @@ function parseArgs()
 
     export BITS
     export YUV_FMT
+    export FRAMERATE
+    export DELAY
 
     export S_TYPE="u$BITS"
 
-    export FFMPEG_IN_OPTS
-    export FFMPEG_OUT_OPTS
     export UNUSED_ARGS
+
+    # FFMPEG_OUT_OPTS is no longer needed for GIF creation
 }
 
 function cmd()
@@ -192,26 +195,26 @@ function cmdSilent()
 
 function getResolution()
 {
-    eval $(cmd ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=height,width \"$1\")
-    RES="${streams_stream_0_width}${2}${streams_stream_0_height}"
+    eval $(cmd ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=height,width "$1")
+    RES="${streams_stream_0_width}x${streams_stream_0_height}"
     echo "$RES"
 }
 
 function getFrames()
 {
-    FRAMES=$(cmd ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=noprint_wrappers=1:nokey=1 \"$1\")
+    FRAMES=$(cmd ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=noprint_wrappers=1:nokey=1 "$1")
     REGEXP_INTEGER='^[0-9]+$'
     if ! [[ $FRAMES =~ $REGEXP_INTEGER ]] ; then
         echo ""
         return 0
     fi
-    echo "-frames $FRAMES"
+    echo "$FRAMES"
     return 0
 }
 
 function getAudio()
 {
-    AUDIO=$(cmd ffprobe -i \"$1\" -show_streams -select_streams a -loglevel error)
+    AUDIO=$(cmd ffprobe -i "$1" -show_streams -select_streams a -loglevel error)
     [[ $AUDIO = *[!\ ]* ]] && echo "-i $TMP_DIR/audio_out.${AUDIO_TYPE}"
 }
 
@@ -225,55 +228,70 @@ function checkDependencies()
     done
 }
 
-checkDependencies ffprobe ffmpeg sox tr
+checkDependencies ffprobe ffmpeg sox magick tr
 parseArgs "$@"
 
 # Create a unique temporary directory within the script's directory
 TMP_DIR=$(mktemp -d "${SCRIPT_DIR}/tmp_audio_shop_XXXXXX")
 
 AUDIO_TYPE="mp3"
-RES=${RES:-"$(getResolution "$1" x)"}
-VIDEO=${VIDEO:-"$(getFrames "$1")"}
+RES=${RES:-"$(getResolution "$1")"}
+FRAMES=${FRAMES:-"$(getFrames "$1")"}
 AUDIO=${AUDIO:-"$(getAudio "$1")"}
 
 echo "TMP_DIR:         $TMP_DIR"
 echo "RES:             $RES"
-echo "VIDEO:           $VIDEO"
+echo "FRAMES:          $FRAMES"
 echo "AUDIO:           $AUDIO"
-echo "FFMPEG_IN_OPTS:  $(eval echo "$FFMPEG_IN_OPTS")"
-echo "FFMPEG_OUT_OPTS: $(eval echo "$FFMPEG_OUT_OPTS")"
-echo "SOX_OPTS:        $(eval echo "$SOX_OPTS")"
+echo "SOX_OPTS:        $SOX_OPTS"
 
 echo "Extracting raw image data.."
-cmdSilent "ffmpeg -y -i \"$1\" -pix_fmt $YUV_FMT $FFMPEG_IN_OPTS  $TMP_DIR/tmp.yuv"
+# Extract frames as PNGs for ImageMagick
+cmdSilent "ffmpeg -y -i \"$1\" -pix_fmt rgba -vf scale=${RES} $TMP_DIR/frame_%04d.png"
 
 [[ $AUDIO = *[!\ ]* ]] && echo "Extracting audio track.."
 [[ $AUDIO = *[!\ ]* ]] && cmdSilent "ffmpeg -y -i \"$1\" -q:a 0 -map a $TMP_DIR/audio_in.${AUDIO_TYPE}"
 
-echo "Processing as sound.."
-mv "$TMP_DIR"/tmp.yuv "$TMP_DIR"/tmp_audio_in."$S_TYPE"
-cmdSilent sox --bits "$BITS" -c1 -r44100 --encoding unsigned-integer -t "$S_TYPE" "$TMP_DIR"/tmp_audio_in."$S_TYPE"  \
-              --bits "$BITS" -c1 -r44100 --encoding unsigned-integer -t "$S_TYPE" "$TMP_DIR"/tmp_audio_out."$S_TYPE" \
-              "$SOX_OPTS"
+echo "Processing image data as sound.."
+# Convert PNG frames to raw audio data
+for frame in "$TMP_DIR"/frame_*.png; do
+    magick "$frame" -flatten -alpha off -depth "$BITS" gray:"${frame%.png}.gray"
+done
+
+# Concatenate all grayscale frames into a single raw audio file
+cat "$TMP_DIR"/*.gray > "$TMP_DIR"/tmp_audio_in."$S_TYPE"
+
+cmdSilent sox --bits "$BITS" -c1 -r44100 --encoding unsigned-integer -t "$S_TYPE" \
+    "$TMP_DIR"/tmp_audio_in."$S_TYPE" \
+    --bits "$BITS" -c1 -r44100 --encoding unsigned-integer -t "$S_TYPE" \
+    "$TMP_DIR"/tmp_audio_out."$S_TYPE" \
+    "$SOX_OPTS" \
+    trim 0
 
 [[ $AUDIO = *[!\ ]* ]] && echo "Processing audio track as sound.."
-[[ $AUDIO = *[!\ ]* ]] && cmdSilent sox "$TMP_DIR"/audio_in.${AUDIO_TYPE}  \
-                                        "$TMP_DIR"/audio_out.${AUDIO_TYPE} \
+[[ $AUDIO = *[!\ ]* ]] && cmdSilent sox "$TMP_DIR"/audio_in."${AUDIO_TYPE}"  \
+                                        "$TMP_DIR"/audio_out."${AUDIO_TYPE}" \
                                         "$SOX_OPTS"
 
-echo "Recreating image data from audio.."
-cmdSilent ffmpeg -y \
-                 "$(eval echo $FFMPEG_OUT_OPTS)" \
-                 -f rawvideo -pix_fmt $YUV_FMT -s $RES \
-                 -i $TMP_DIR/tmp_audio_out.$S_TYPE \
-                 $AUDIO \
-                 $VIDEO \
-                 \"$2\"
+echo "Recreating image frames from processed audio.."
+# Split the processed audio back into grayscale frames
+sox "$TMP_DIR"/tmp_audio_out."$S_TYPE" -c 1 -r 44100 "$TMP_DIR"/processed_audio.wav remix 1 channels 1
 
-# [[ $AUDIO = *[!\ ]* ]] && echo "Injecting modified audio.."
-# [[ $AUDIO = *[!\ ]* ]] && cmdSilent ffmpeg -y \
-#                                           -i \"$2\" \
-#                                           $AUDIO \
-#                                           \"$2\"
+# For simplicity, let's assume one audio frame corresponds to one image frame
+# Here, we'll generate new PNG frames based on the processed audio
+# This part may need customization based on how audio affects the image
+
+# Example: Adjust brightness based on audio amplitude
+sox "$TMP_DIR"/processed_audio.wav -n stat 2>&1 | grep "RMS" | awk '{print $3}' > "$TMP_DIR"/audio_rms.txt
+RMS=$(cat "$TMP_DIR"/audio_rms.txt)
+for frame in "$TMP_DIR"/frame_*.png; do
+    magick "$frame" -modulate $((100 + RMS * 100)) "$TMP_DIR"/processed_"$(basename "$frame")"
+done
+
+echo "Creating animated GIF with ImageMagick.."
+# Assemble processed frames into an animated GIF
+magick convert -delay "$DELAY" -dispose previous -loop 0 "$TMP_DIR"/processed_frame_*.png -coalesce -layers OptimizeTransparency "$2"
+
+echo "Animated GIF created at $2"
 
 cleanup 0
